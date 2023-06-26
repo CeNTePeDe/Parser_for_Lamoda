d9cb83af2e3ebd9085c6725056c3d22d89e81286
Olga Orlova <108580276+CeNTePeDe@users.noreply.github.com>
2023-06-26 18:55:10 +0300

    added: -get_filter_products, delete_products to ProductDAO -get_filter_products, delete_collection to controller

--- data_base/database.py
+++ data_base/database.py
@@ -4,7 +4,7 @@ from pydantic import AnyUrl
 from pymongo import MongoClient
 from pymongo.collection import Collection
 
-from schema.schemas import serialize_products_with_url
+from schema.schemas import serialize_products_with_url, serialize_list_of_products
 
 logger = logging.getLogger(__name__)
 
@@ -17,11 +17,12 @@ class ProductDAO:
         self.db = self.client["product_db"]
         self.collection = self.db["products"]
 
-    def insert_products(self, url: AnyUrl, products: list[dict]) -> None:
+    def create_products(self, url: AnyUrl, products: list[dict]) -> None:
         logger.info("run insert function")
         for product in products:
             self.collection.update_one({'url': url}, {'$push': {'products': product}}, upsert=True)
         logger.info(f"data_structure {products}")
+
 
     def get_products(self, url: AnyUrl) -> dict:
         products = self.collection.find({"url": url})
@@ -30,3 +31,18 @@ class ProductDAO:
             product_item = serialize_products_with_url(item)
             return product_item
 
+    def get_filter_products(self, url: AnyUrl, min_price: float, max_price: float):
+        products = self.collection.find({"url": url})
+        for item in products:
+            list_products = item.get("products")
+            logger.info(f"list of products {list_products}")
+            sorted_product_list = []
+            for product_item in list_products:
+                if min_price < float(product_item["price"].split()[0]) < max_price:
+                    sorted_product_list.append(product_item)
+                    logger.info(f"{sorted_product_list}")
+            return serialize_list_of_products(sorted_product_list)
+
+    def delete_products(self, url: AnyUrl):
+        result = self.collection.delete_many({"url": url})
+        logger.info(f"Deleted {result.deleted_count} documents")
--- routers/product.py
+++ routers/product.py
@@ -14,9 +14,19 @@ product_dao = ProductDAO()
 
 
 @routers.post("/parse", status_code=status.HTTP_200_OK)
-def parse_func(url: AnyUrl):
+def get_products(url: AnyUrl):
     logger.info("get url")
     products = asyncio.run(gather_data(url))
-    product_dao.insert_products(url, products)
+    product_dao.create_products(url, products)
     return product_dao.get_products(url)
 
+
+@routers.get("/parse/filter", status_code=status.HTTP_200_OK)
+def get_filter_products(url: AnyUrl, min_price: float, max_price: float):
+    return product_dao.get_filter_products(url, min_price=min_price, max_price=max_price)
+
+
+@routers.delete("/delete", status_code=status.HTTP_204_NO_CONTENT)
+def delete_collection(url: AnyUrl):
+    product_dao.delete_products(url)
+    return {"message": "Document was deleted"}
